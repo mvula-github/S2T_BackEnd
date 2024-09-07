c593b6402f23ffda3375122bda705553b042a12d
mvula <mvulakolweni08@gmail.com>
2024-09-07 22:57:02 +0200

    added some testing data on faqData file

---
+++ src/utils/FAQ/faqData.mjs
@@ -0,0 +1,34 @@
+export const faqList = [
+  {
+    id: 1,
+    category: "Contribute",
+    question: "How to upload",
+    answer: "click the upload button",
+  },
+  {
+    id: 2,
+    category: "Contribute",
+    question: "How do I know my document is approved",
+    answer: "You will get a notification once your docuent is approved or not",
+  },
+  {
+    id: 3,
+    category: "Document",
+    question: "Where can I find subjects",
+    answer:
+      "You can either search your desired subject or go to the subject page",
+  },
+  {
+    id: 4,
+    category: "Account",
+    question: "Who can create an account",
+    answer: "Only teacher with varified documents can create accounts",
+  },
+  {
+    id: 5,
+    category: "Account",
+    question: "How do I reset my password",
+    answer:
+      "On the login page, there is a link that will redirect you to the reset password page, but your password will be reset of you have an account with the platform",
+  },
+];
