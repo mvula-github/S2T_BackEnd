31f05efe17385df64cb0ad0f97e1562ff7770ab7
mvula <mvulakolweni08@gmail.com>
2024-09-08 01:40:37 +0200

    created a get method with query, for filtering the faq(not currently working need to dix logic)

--- src/routes/faqRouter.mjs
+++ src/routes/faqRouter.mjs
@@ -8,4 +8,20 @@ faq.get("/api/faqs", (request, response) => {
   response.status(200).send(faqList);
 });
 
+//query request method (i.e. filter)
+faq.get("/api/faqs", (request, response) => {
+  console.log(request.query);
+  const {
+    query: { filter, value },
+  } = request;
+  //if (!filter && !value) return response.send(faqList); // return all faq list because its undefined
+
+  if (filter && value)
+    return response.send(
+      faqList.filter((item) => item[filter].includes(value))
+    );
+
+  //return response.send(faqList);
+});
+
 export default faq;
--- src/utils/FAQ/faqData.mjs
+++ src/utils/FAQ/faqData.mjs
@@ -1,32 +1,32 @@
 export const faqList = [
   {
     id: 1,
-    category: "Contribute",
+    category: "contribute",
     question: "How to upload",
     answer: "click the upload button",
   },
   {
     id: 2,
-    category: "Contribute",
+    category: "contribute",
     question: "How do I know my document is approved",
     answer: "You will get a notification once your docuent is approved or not",
   },
   {
     id: 3,
-    category: "Document",
+    category: "document",
     question: "Where can I find subjects",
     answer:
       "You can either search your desired subject or go to the subject page",
   },
   {
     id: 4,
-    category: "Account",
+    category: "account",
     question: "Who can create an account",
     answer: "Only teacher with varified documents can create accounts",
   },
   {
     id: 5,
-    category: "Account",
+    category: "account",
     question: "How do I reset my password",
     answer:
       "On the login page, there is a link that will redirect you to the reset password page, but your password will be reset of you have an account with the platform",
