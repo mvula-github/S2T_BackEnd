936578e203e229639a3aceb44e37c3888e0eef46
mvula <mvulakolweni08@gmail.com>
2024-09-26 06:47:18 +0200

    updated routers and schemas for file upload and storage

--- src/mongoose/schemas/fileStorage.mjs
+++ src/mongoose/schemas/fileStorage.mjs
@@ -1,27 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const fileSchema = new mongoose.Schema({
-    originalName: {
-        type: String,
-        required: true
-    },
-    filePath: {
-        type: String,
-        required: true
-    },
-    size: {
-        type: Number,
-        required: true
-    },
-    mimetype: {
-        type: String,
-        required: true
-    },
-    uploadedAt: {
-        type: Date,
-        default: Date.now
-    }
+const storageSchema = new mongoose.Schema({
+  originalName: {
+    type: String,
+    required: true,
+  },
+  filePath: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  mimetype: {
+    type: String,
+    required: true,
+  },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const File = mongoose.model('File', fileSchema);
+const Storage = mongoose.model("Storage", storageSchema);
 export default File;
--- src/mongoose/schemas/fileUpload.mjs
+++ src/mongoose/schemas/fileUpload.mjs
@@ -1,32 +1,33 @@
 import mongoose from "mongoose";
 
-const fileSchema = new mongoose.Schema({
-    userFile: {
-        type: String,  // Path of the file saved on the server
-        required: true
-    },
-    fileType: {
-        type: String,
-        required: true,
-        enum: ["pdf", "doc", "docx", "jpeg", "png"] // Limit allowed file types
-    },
-    fileName: {
-        type: String,
-        required: true
-    },
-    subject: {
-        type: String,
-        required: true
-    },
-    grade: {
-        type: String,
-        required: true
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now
-    }
+const uploadSchema = new mongoose.Schema({
+  userFile: {
+    type: String, // Path of the file saved on the server
+    required: true,
+  },
+  fileType: {
+    type: String,
+    required: true,
+    enum: ["pdf"], // Limit allowed file types
+  },
+  fileName: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  //add SIZE, PATH, EXTENTION, AUTHOR, TAGS, DESCRIPTION
 });
 
-const File = mongoose.model("File", fileSchema);
-export default File;
+const Upload = mongoose.model("Upload", uploadSchema);
+export default Upload;
