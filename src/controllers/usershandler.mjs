a7b882ddc11adc731b77432b15fcf7a48d0a907a
mvula <mvulakolweni08@gmail.com>
2024-10-06 23:34:45 +0200

    moved all the userRouter code to the usersController file

--- src/controllers/usershandler.mjs
+++ src/controllers/usershandler.mjs
@@ -1,14 +1,85 @@
 import { User } from "../mongoose/schemas/user.mjs";
 import { requireAuth } from "../utils/middleware/middleware.mjs";
+import { validationResult, matchedData } from "express-validator";
+import { NotFoundError, UnauthorizedError } from "../utils/classes/errors.mjs";
 
-export const getAllUsers = async (request, response) => {
+export const getAllUsers = async (request, response, next) => {
+  if (!requireAuth) throw new UnauthorizedError("User not logged in"); //verify if user is logged in
+
   try {
-    if (!requireAuth) return response.status(401).send("User not logged in"); //verify if user is logged in
-
     const allUsers = await User.find();
 
     return response.status(200).send(allUsers);
   } catch (err) {
-    return request.response.send(`${err} `);
+    next(`${err} `);
+  }
+};
+
+export const getUserById = async (request, response, next) => {
+  if (!requireAuth) throw new UnauthorizedError("User not logged in"); //verify if user is logged in
+
+  const {
+    params: { id },
+  } = request;
+
+  try {
+    const theUser = await User.findById(id);
+    if (!theUser) throw new NotFoundError("User not found");
+
+    response.status(200).send(theUser);
+  } catch (err) {
+    next(`${err} `);
+  }
+};
+
+export const updateUserById = async (request, response, next) => {
+  if (!requireAuth) throw new UnauthorizedError("User not logged in"); //verify if user is logged in
+
+  //handle the validations i.e. doesn't add invalid data to database
+  const errors = validationResult(request);
+  if (!errors.isEmpty())
+    return response.status(400).send(errors.array().map((err) => err.msg));
+  //console.log({ error: errors.array() }); To nvaigate the error precisely
+
+  //To know if the data is valid or not
+  const {
+    params: { id },
+  } = request;
+
+  try {
+    //extracting and storing only valid data
+    const data = matchedData(request);
+    const updatedUser = await User.findByIdAndUpdate(id, data);
+
+    if (!updatedUser) throw new NotFoundError("User not found");
+
+    response.status(201).send("User updated successlly");
+  } catch (err) {
+    next(`${err} `);
+  }
+};
+
+export const deleteUserById = async (request, response, next) => {
+  try {
+    if (!requireAuth) throw new UnauthorizedError("User not logged in"); //verify if user is logged in
+
+    const errors = validationResult(request);
+    //handle the validations
+    if (!errors.isEmpty())
+      return response.status(400).send(errors.array().map((err) => err.msg));
+
+    //console.log({ error: errors.array() }); To nvaigate the error precisely
+
+    const {
+      params: { id },
+    } = request;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) throw new NotFoundError("User not found");
+
+    response.status(200).send("User removed successfully");
+  } catch (err) {
+    next(`${err} `);
   }
 };
--- src/routes/usersRouter.mjs
+++ src/routes/usersRouter.mjs
@@ -1,18 +1,13 @@
 import express, { Router } from "express";
-import { User } from "../mongoose/schemas/user.mjs";
-import { validationResult, matchedData, checkSchema } from "express-validator";
-import {
-  addUserValidation,
-  updateUserValidation,
-} from "../utils/validation/usersValidation.mjs";
-import {
-  NotFoundError,
-  ValidationError,
-  UnauthorizedError,
-  ForbiddenError,
-} from "../utils/classes/errors.mjs";
+import { checkSchema } from "express-validator";
+import { updateUserValidation } from "../utils/validation/usersValidation.mjs";
 import { requireAuth } from "../utils/middleware/middleware.mjs";
-import { getAllUsers } from "../controllers/usershandler.mjs";
+import {
+  deleteUserById,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+} from "../controllers/usershandler.mjs";
 
 const app = Router();
 
@@ -26,115 +21,20 @@ app.get("/api/users", requireAuth, getAllUsers);
 //to find users based in a filter query
 
 //TO RETRIEVE A SPECIFIC USER BY ID
-app.get("/api/users/:id", async (request, response, next) => {
-  try {
-    if (!request.session.user)
-      throw new UnauthorizedError("User not logged in"); //verify if user is logged in
+app.get("/api/users/:id", requireAuth, getUserById);
 
-    const {
-      params: { id },
-    } = request;
-
-    const theUser = await User.findById(id);
-    if (!theUser) throw new NotFoundError("User not found");
-
-    response.status(200).send(theUser);
-  } catch (err) {
-    next(`${err} `);
-  }
-});
-
-//----------------------------------PATCH--------------------------------------
+//----------------------------------UPDATE--------------------------------------
 
 //WHEN USER OR ADMIN WANTS TO UPDATE CERTAIN
 app.patch(
   "/api/users/:id",
+  requireAuth,
   checkSchema(updateUserValidation),
-  async (request, response, next) => {
-    try {
-      if (!request.session.user)
-        throw new UnauthorizedError("User not logged in"); //verify if user is logged in
-
-      //handle the validations i.e. doesn't add invalid data to database
-      if (!errors.isEmpty())
-        return response.status(400).send({ error: errors.array() });
-
-      //To know if the data is valid or not
-      const {
-        params: { id },
-      } = request;
-
-      //extracting and storing only valid data
-      const data = matchedData(request);
-      const updatedUser = await User.findByIdAndUpdate(id, data);
-
-      if (!updatedUser) throw new NotFoundError("User not found");
-
-      response.status(201).send("User updated successlly");
-    } catch (err) {
-      next(`${err} `);
-    }
-  }
-);
-
-//-------------------------------------------PUT--------------------------------------------
-
-app.put(
-  "/api/users/:id",
-  checkSchema(addUserValidation),
-  async (request, response, next) => {
-    try {
-      if (!request.session.user)
-        throw new UnauthorizedError("User not logged in"); //verify if user is logged in
-
-      //handle the validations i.e. doesn't add invalid data to database
-      if (!errors.isEmpty())
-        return response.status(400).send({ error: errors.array() });
-
-      //To know if the data is valid or not
-      const {
-        params: { id },
-      } = request;
-
-      //extracting and storing only valid data
-      const data = matchedData(request);
-      const updatedUser = await User.findByIdAndUpdate(id, data);
-
-      if (!updatedUser) throw new NotFoundError("User not found");
-
-      response.status(201).send("User updated successlly");
-    } catch (err) {
-      next(`${err} `);
-    }
-  }
+  updateUserById
 );
 
 //-------------------------------------------DELETE--------------------------------------------
 
-app.delete("/api/users/:id", async (request, response, next) => {
-  //handle the validations
-
-  try {
-    if (!request.session.user)
-      throw new UnauthorizedError("User not logged in"); //verify if user is logged in
-
-    const errors = validationResult(request);
-    //handle the validations
-    if (!errors.isEmpty())
-      return response.status(400).send({ error: errors.array() });
-
-    const {
-      params: { id },
-    } = request;
-
-    const deletedUser = await User.findByIdAndDelete(id);
-
-    if (!deletedUser) throw new NotFoundError("User not found");
-
-    response.status(200).send("User removed successfully");
-  } catch (err) {
-    next(`${err} `);
-  }
-});
+app.delete("/api/users/:id", requireAuth, deleteUserById);
 
 export default app;
